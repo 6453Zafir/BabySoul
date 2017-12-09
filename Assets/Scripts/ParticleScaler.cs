7fba9f7850e2a378b4c9b18aa6313800706bf81b
Whistlle <a1612211@126.com>
2017-12-10 01:55:18 +0800

    更新人物火焰粒子特效 ，自爆动画

---
+++ Assets/Scripts/ParticleScaler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParticleScaler:MonoBehaviour
+{
+    public float Scale = 1;
+    public void ScaleParticleSystem()
+    {
+        var scale = Scale;
+        var gameObj = this;
+        var hasParticleObj = false;
+        var particles = gameObj.GetComponentsInChildren<ParticleSystem>(true);
+        var max = particles.Length;
+        for (int idx = 0; idx < max; idx++)
+        {
+            var particle = particles[idx];
+            if (particle == null) continue;
+            hasParticleObj = true;
+            particle.startSize *= scale;
+            particle.startSpeed *= scale;
+            particle.startRotation *= scale;
+            particle.transform.localScale *= scale;
+        }
+        if (hasParticleObj)
+        {
+            gameObj.transform.localScale = new Vector3(scale, scale, 1);
+        }
+    }
+}
