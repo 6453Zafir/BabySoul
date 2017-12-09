3ed9bb018e991bfc47719249105ef9faa26b6281
Evan <cpp971019@gmail.com>
2017-12-09 16:33:26 +0800

    Update of Spear

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
+	enum SKILLSELECTED{
+		NONE,
+		Granade,
+		Spear,
+		Suicide
+	}
 	public GameObject granade_prefab;
+	public GameObject spear_prefab;
+	SKILLSELECTED skill_selected = SKILLSELECTED.NONE;
     public float moveSpeed = 5f;
     bool isBabyMoving = false;
 	// Use this for initialization
@@ -28,26 +36,58 @@ public class Controller : MonoBehaviour {
 			this.transform.position += new Vector3 (moveSpeed, 0, 0) * Time.deltaTime;
             isBabyMoving = true;
         }
-
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			skill_selected = SKILLSELECTED.Granade;
+		}
+		if (Input.GetKey (KeyCode.Alpha2)) {
+			skill_selected = SKILLSELECTED.Spear;
+		}
+		if (Input.GetKey (KeyCode.Alpha3)) {
+			skill_selected = SKILLSELECTED.Suicide;
+		}
 
 
         if (Input.GetMouseButtonDown(0) && UIController.isThrowingGa )
         {
-            if (UIController.GranadeLeft > 0) {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit raycastHit = new RaycastHit();
-                if (Physics.Raycast(ray, out raycastHit))
-                {
-                    Vector3 pos = raycastHit.point;
-                    GameObject armo = Instantiate(granade_prefab, this.transform.position, this.transform.rotation);
-                    armo.GetComponent<GranadeLogic>().SendMessage("Throw", pos);
-                }
-                UIController.GranadeLeft -= 1;
-            }
-            else
-            {
-                print("没有炸弹了！");
-            }
+			switch (skill_selected) {
+			case SKILLSELECTED.Granade:
+				if (UIController.GranadeLeft > 0) {
+					Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+					RaycastHit raycastHit = new RaycastHit();
+					if (Physics.Raycast(ray, out raycastHit))
+					{
+						Vector3 pos = raycastHit.point;
+						GameObject armo = Instantiate(granade_prefab, this.transform.position, this.transform.rotation);
+						armo.GetComponent<GranadeLogic>().SendMessage("Throw", pos);
+					}
+					UIController.GranadeLeft -= 1;
+				}
+				else
+				{
+					print("没有炸弹了！");
+				}
+				break;
+			case SKILLSELECTED.Spear:
+				if (UIController.ArrowLeft > 0) {
+					Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+					RaycastHit raycastHit = new RaycastHit();
+					if (Physics.Raycast(ray, out raycastHit))
+					{
+						Vector3 pos = raycastHit.point;
+						GameObject armo = Instantiate(spear_prefab, this.transform.position, this.transform.rotation);
+						armo.GetComponent<SpearLogic>().SendMessage("Throw", pos);
+					}
+					UIController.GranadeLeft -= 1;
+				}
+				else
+				{
+					print("没有长枪了！");
+				}
+				break;
+			default:
+				break;
+			}
+            
         }
 
 	}
---
+++ Assets/Scripts/SpearLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearLogic : MonoBehaviour {
+	Vector3 move_direction;
+
+	// Use this for initialization
+	void Start () {
+		Throw (new Vector3 (1, 0, 2));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += move_direction * Time.deltaTime;
+	}
+
+	public void Throw(Vector3 pos){
+		move_direction = 30 * Vector3.Normalize (pos - this.transform.position);
+		this.transform.LookAt (pos);
+	}
+}
