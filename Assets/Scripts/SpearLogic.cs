c66bbde1e9e7952a644f4ab7f791107a82200790
Evan <cpp971019@gmail.com>
2017-12-09 17:04:00 +0800

    Update of spear logic

--- Assets/Scripts/SpearLogic.cs
+++ Assets/Scripts/SpearLogic.cs
@@ -12,6 +12,13 @@ public class SpearLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.transform.position += move_direction * Time.deltaTime;
+		Ray ray = new Ray (this.transform.position, this.transform.position + move_direction * Time.deltaTime);
+		RaycastHit raycastHit = new RaycastHit();
+		if (Physics.Raycast (ray, out raycastHit, 1.5f)) {
+			if (string.Equals (raycastHit.transform.gameObject.name, "Enemy(Clone)")) {
+				Destroy (raycastHit.transform.gameObject);
+			}
+		}
 	}
 
 	public void Throw(Vector3 pos){
