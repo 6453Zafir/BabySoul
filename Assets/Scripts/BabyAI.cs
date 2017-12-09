7961f70cbb5907090d0eafd5b10ff9dabaca18cf
6453Zafir <511599737@qq.com>
2017-12-09 18:09:22 +0800

    add collision

--- Assets/Scripts/BabyAI.cs
+++ Assets/Scripts/BabyAI.cs
@@ -18,6 +18,7 @@ public class BabyAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(Camera.main.transform.position, Vector3.up);
+        BabyHealth += 10;
         if (Vector3.Distance (this.transform.position, target_list [index].position) <= 1.0f && index < target_list.Length - 1) {
 			index++;
 			agent.SetDestination (target_list [index].position);
