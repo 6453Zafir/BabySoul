e92e6e578840c627c41aeb64eaa9ce72a2e6dac0
Evan <cpp971019@gmail.com>
2017-12-09 21:55:16 +0800

    Fixed bugs

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -67,7 +67,7 @@ public class Controller : MonoBehaviour {
 				}
 				break;
 			case SKILLSELECTED.Spear:
-				if (UIController.ArrowLeft > -10) {
+				if (UIController.ArrowLeft > 0) {
 					Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 					RaycastHit raycastHit = new RaycastHit();
 					if (Physics.Raycast(ray, out raycastHit))
